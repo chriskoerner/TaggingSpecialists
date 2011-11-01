c0212219b553c75cb2c208f87b66149e246cce02
Karin Schoefegger <k.schoefegger@gmail.com>
2011-11-01 16:57:58 +0200

    updated lookup utilities

--- lookup_utilities.py
+++ lookup_utilities.py
@@ -5,7 +5,6 @@ __author__ = 'kschoef'
 import collections
 from TagDictionary import TagDictionary
 
-
 def user_academicstatus_lookup():
   file_ = open('data/user_academicStatusId')
   userprof_dict = {}
@@ -19,7 +18,7 @@ def user_academicstatus_lookup():
 	  prof = int(row[1])
 	else: prof = -1	
 	userprof_dict[user]=prof
-   return userprof_dict
+  return userprof_dict
 
 
 def user_discipline_lookup():            
@@ -37,6 +36,31 @@ def user_discipline_lookup():
   return userdiscipline_dict, userowndiscipline_dict
   
   
+def relevantuserdisc_lookup(threshold=0.0):
+  """ returns 2 values: a dict with the number of relevat automatically detected disciplines for each user 
+  and the id of the self-added discipline for each user"""
+  file_ = open('ground/users_distribution_overDiscipline.via_resources')
+  count_abweichung = 0
+  userdiscipline_dict = defaultdict(list)
+  userowndiscipline_dict = defaultdict(str)
+  for line in file_: 
+      row = line.strip('\n').split('\t')
+      user = row[0]
+      disciplines = json.loads(row[1])
+      di = row[2]
+      userowndiscipline_dict[user] = di
+      #calculate
+      if (threshold > 0.0 and threshold <= 1.0):
+	m = max(disciplines.values())
+	for key in disciplines:
+	  if disciplines[key] < threshold*m:
+	    del disciplines[key]
+      userdiscipline_dict[user] = len(disciplines)
+  return userdiscipline_dict, userowndiscipline_dict
+  
+
+  
+  
 def user_nrresources_lookup(): 
   file_ = open('data/user_resources_count')
   dict_userres = collections.defaultdict(int)
@@ -50,7 +74,7 @@ def user_nrresources_lookup():
   
   
   
-def read_normalizedTagGeneralityData()
+def read_normalizedTagGeneralityData():
     TD = TagDictionary()
 
     file_ = open('data/degree_tag')
@@ -96,3 +120,23 @@ def read_normalizedTagGeneralityData()
     TD.normalize_dict()	 
     
     return TD	   
+    
+    
+    
+def read_extended_personomy_file(line):
+      row = line.strip('\n').split(' ')
+      freq = float(row[0])
+      username = row[1]
+      #if (new_username == '1165'): print line
+      tag = row[2]
+      if not(row[3] == 'None'): tag_fu = float(row[3])
+      else: tag_fu = None
+      if not(row[4] == 'None'): tag_fr = float(row[4])
+      else: tag_fr = None
+      if not(row[5] == 'None'): tag_d = float(row[5])
+      else: tag_d = None
+      if not(row[6] == 'None'): tag_eu = float(row[6])
+      else: tag_eu = None
+      if not(row[7] == 'None'): tag_t = float(row[7])
+      else: tag_t = None
+      return username, freq, tag_fr, tag_fu, tag_eu, tag_t, tag_d
